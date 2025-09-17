16b073fdc82f93536be78e3d4ae1f238fc72de23
4pd4stantone <victor.stanton@gmail.com>
2025-09-17 09:02:28 -0400

    added a table and 2 dropdown menus

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 
 
+// const fragZone = document.getElementById('coolFragZone')
+
+// const fragment = document.createDocumentFragment();
+// fragment.appendChild(document.createElement)
 
 
+// adding weeks and sets to workout plan
+const weeks = 5;
+const sets = 5;
 
+const workoutTable = document.getElementById("workout-table");
+const table = document.createElement("table");
 
+for (let r = 0; r < weeks; r++) {
+  const tr = document.createElement("tr");
+  for (let s = 0; s < sets; s++) {
+    const td = document.createElement("td");
+    tr.appendChild(td);
+  }
+  table.appendChild(tr);
+}
 
-
-
-
-const fragZone = document.getElementById('coolFragZone')
-
-const fragment = document.createDocumentFragment();
-fragment.appendChild(document.createElement)
+workoutTable.appendChild(table);
+console.log(table)
